e11c22bf5ef3c02f1f8e1e21eaf4f5504bf3cf67
James Wilson <j.w@jameswilson.io>
2025-02-12 10:19:50 +1100

    Removed astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx(), alpinejs()],
+  integrations: [tailwind(), mdx(), alpinejs()],
 });
